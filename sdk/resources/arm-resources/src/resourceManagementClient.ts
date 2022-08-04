6102cfd0fcefe41940eb8c85fb9e909071d84296
SDKAuto <sdkautomation@microsoft.com>
2022-08-04 19:31:16 +0000

    CodeGen from PR 20117 in Azure/azure-rest-api-specs Merge db4c6ab1a0d5a30247c945979ba18896af12908d into 6f0c7d58c0a923917c2b3467ee756f21dbd2f8e2

--- sdk/resources/arm-resources/src/resourceManagementClient.ts
+++ sdk/resources/arm-resources/src/resourceManagementClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   OperationsImpl,
@@ -63,7 +68,7 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-resources/5.0.2`;
+    const packageDetails = `azsdk-js-arm-resources/5.1.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -119,6 +124,35 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
     this.resourceGroups = new ResourceGroupsImpl(this);
     this.tagsOperations = new TagsOperationsImpl(this);
     this.deploymentOperations = new DeploymentOperationsImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return "api-version=" + apiVersion;
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   operations: Operations;
