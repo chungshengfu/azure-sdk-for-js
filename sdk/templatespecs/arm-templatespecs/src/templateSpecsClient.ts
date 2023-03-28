d20eaa679bebf725234b4ef702045c53e8cb5673
SDKAuto <sdkautomation@microsoft.com>
2023-03-28 06:58:01 +0000

    CodeGen from PR 23316 in Azure/azure-rest-api-specs Merge 2bd114f823323acf07d9ec9b683bc6dd6206a0e1 into f66499b77cd90144ceb0d118c1bd8bda7dbd4cfc

--- sdk/templatespecs/arm-templatespecs/src/templateSpecsClient.ts
+++ sdk/templatespecs/arm-templatespecs/src/templateSpecsClient.ts
@@ -50,7 +50,7 @@ export class TemplateSpecsClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-templatespecs/2.1.1`;
+    const packageDetails = `azsdk-js-arm-templatespecs/3.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -103,7 +103,7 @@ export class TemplateSpecsClient extends coreClient.ServiceClient {
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2021-05-01";
+    this.apiVersion = options.apiVersion || "2022-02-01";
     this.templateSpecs = new TemplateSpecsImpl(this);
     this.templateSpecVersions = new TemplateSpecVersionsImpl(this);
     this.addCustomApiVersionPolicy(options.apiVersion);
