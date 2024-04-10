edae728fc2864ea065533d7bd6ce25c4996827a3
SDKAuto <sdkautomation@microsoft.com>
2024-04-10 05:14:38 +0000

    CodeGen from PR 28645 in Azure/azure-rest-api-specs Merge 4b8e024312cb4969ccce3227952e0ee5f09328c1 into c77bbf822be2deaac1b690270c6cd03a52df0e37

--- sdk/containerservice/arm-containerservicefleet/src/models/mappers.ts
+++ sdk/containerservice/arm-containerservicefleet/src/models/mappers.ts
@@ -234,7 +234,6 @@ export const FleetListResult: coreClient.CompositeMapper = {
       },
       nextLink: {
         serializedName: "nextLink",
-        readOnly: true,
         type: {
           name: "String",
         },
@@ -580,7 +579,6 @@ export const FleetMemberListResult: coreClient.CompositeMapper = {
       },
       nextLink: {
         serializedName: "nextLink",
-        readOnly: true,
         type: {
           name: "String",
         },
@@ -629,7 +627,6 @@ export const UpdateRunListResult: coreClient.CompositeMapper = {
       },
       nextLink: {
         serializedName: "nextLink",
-        readOnly: true,
         type: {
           name: "String",
         },
@@ -1103,7 +1100,6 @@ export const FleetUpdateStrategyListResult: coreClient.CompositeMapper = {
       },
       nextLink: {
         serializedName: "nextLink",
-        readOnly: true,
         type: {
           name: "String",
         },
@@ -1327,16 +1323,16 @@ export const FleetsUpdateHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "FleetsUpdateHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1348,16 +1344,16 @@ export const FleetsDeleteHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "FleetsDeleteHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1384,16 +1380,16 @@ export const FleetMembersUpdateHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "FleetMembersUpdateHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1405,16 +1401,16 @@ export const FleetMembersDeleteHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "FleetMembersDeleteHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1441,16 +1437,16 @@ export const UpdateRunsDeleteHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "UpdateRunsDeleteHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1462,16 +1458,16 @@ export const UpdateRunsSkipHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "UpdateRunsSkipHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1483,16 +1479,16 @@ export const UpdateRunsStartHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "UpdateRunsStartHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1504,16 +1500,16 @@ export const UpdateRunsStopHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "UpdateRunsStopHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
@@ -1541,16 +1537,16 @@ export const FleetUpdateStrategiesDeleteHeaders: coreClient.CompositeMapper = {
     name: "Composite",
     className: "FleetUpdateStrategiesDeleteHeaders",
     modelProperties: {
-      retryAfter: {
-        serializedName: "retry-after",
-        type: {
-          name: "Number",
-        },
-      },
       location: {
         serializedName: "location",
         type: {
           name: "String",
+        },
+      },
+      retryAfter: {
+        serializedName: "retry-after",
+        type: {
+          name: "Number",
         },
       },
     },
