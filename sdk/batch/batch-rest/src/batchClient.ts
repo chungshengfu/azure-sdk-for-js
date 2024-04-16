8cccf9a25daf149849949f5963c74452249653e0
SDKAuto <sdkautomation@microsoft.com>
2024-04-16 16:51:49 +0000

    CodeGen from PR 28529 in Azure/azure-rest-api-specs Merge 9f577ffe7144aadb15afe4410d1aadd81d8abc67 into b38632bbd56247985cb0493b128ba048e5fee16b

---
+++ sdk/batch/batch-rest/src/batchClient.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { getClient, ClientOptions } from "@azure-rest/core-client";
+import { logger } from "./logger.js";
+import { TokenCredential } from "@azure/core-auth";
+import { BatchClient } from "./clientDefinitions.js";
+
+/**
+ * Initialize a new instance of `BatchClient`
+ * @param endpointParam - Batch account endpoint (for example: https://batchaccount.eastus2.batch.azure.com).
+ * @param credentials - uniquely identify client credential
+ * @param options - the parameter for all optional parameters
+ */
+export default function createClient(
+  endpointParam: string,
+  credentials: TokenCredential,
+  options: ClientOptions = {},
+): BatchClient {
+  const endpointUrl = options.endpoint ?? options.baseUrl ?? `${endpointParam}`;
+
+  const userAgentInfo = `azsdk-js-batch-rest/1.0.0-beta.1`;
+  const userAgentPrefix =
+    options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+      ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
+      : `${userAgentInfo}`;
+  options = {
+    ...options,
+    userAgentOptions: {
+      userAgentPrefix,
+    },
+    loggingOptions: {
+      logger: options.loggingOptions?.logger ?? logger.info,
+    },
+    credentials: {
+      scopes: options.credentials?.scopes ?? [
+        "https://batch.core.windows.net//.default",
+      ],
+    },
+  };
+
+  const client = getClient(endpointUrl, credentials, options) as BatchClient;
+
+  client.pipeline.removePolicy({ name: "ApiVersionPolicy" });
+
+  return client;
+}
---
+++ sdk/batch/batch-rest/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import BatchClient from "./batchClient.js";
+
+export * from "./batchClient.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+
+export default BatchClient;
